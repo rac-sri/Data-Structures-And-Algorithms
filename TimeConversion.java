2ae68fc659bffce93f7bd0bac0e9f81ed54764f1
rachit2501 <rac.sri25@gmail.com>
2019-12-26 01:03:05 +0530

    TimeConversrion

---
+++ TimeConversion.java
@@ -0,0 +1,35 @@
+package threestar;
+import java.io.*;
+import java.util.*;
+
+public class TimeConversion {
+	
+	static String timeConversion(String s) {
+		String ss = s.substring(s.length()-2);
+		String hr = s.substring(0,2);
+		s = s.substring(2,s.length()-2);
+		int n  = Integer.parseInt(hr);
+		if(ss.equals("PM") && n<12)
+		{
+			n += 12;
+			hr = Integer.toString(n);
+		}
+		else if(ss.contentEquals("AM") && n==12)
+			hr = "00";
+		s = hr + s;
+		System.out.println(s);
+		return s;
+	}
+	
+	private static final Scanner scan = new Scanner(System.in);
+
+	public static void main(String[] args) throws IOException{
+		BufferedWriter bw = new BufferedWriter(new FileWriter("TimeConversion.txt"));
+		String s = scan.nextLine();
+		String result = timeConversion(s);
+		bw.write(result);
+		bw.newLine();
+		bw.close();
+	}
+
+}
