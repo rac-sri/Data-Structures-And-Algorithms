f312a9b18922125b35c91e6b1b37a3048c459d75
rachit2501 <rac.sri25@gmail.com>
2020-04-05 23:23:41 +0530

    simple text editor v1 9/16 test cases

--- simpleTextEditor.java
+++ simpleTextEditor.java
@@ -2,40 +2,26 @@ import java.io.*;
 import java.util.*;
 
 public class simpleTextEditor {
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Stack<String> stack = new Stack<String>();
-        long n = sc.nextInt();
-        String str = "";
-        while (n-- > 0) {
-            int qr = sc.nextInt();
-            String s = "";
-            if (qr != 4)
-                s = sc.nextLine();
-            switch (qr) {
-                case 1:
-                    str += s.trim();
-                    stack.push(str);
-                    break;
-                case 2:
-                    int no = Integer.parseInt(s.trim());
-                    str = str.substring(0, str.length() - no);
-                    stack.push(str);
-                    break;
-                case 3:
-                    int np = Integer.parseInt(s.trim());
-                    System.out.println(str.charAt(np - 1));
-                    break;
-                case 4:
-                    if (!stack.isEmpty()) {
-                        stack.pop();
-                        if (!stack.isEmpty())
-                            str = stack.peek();
-                    }
-                    break;
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        String s = "";
+        Stack<String> st = new Stack<String>();
+        st.push(s);
+        for (int i = 0; i < n; i++) {
+            int t = in.nextInt();
+            if (t == 1) {
+                s = s + in.next();
+                st.push(s);
+            } else if (t == 2) {
+                s = s.substring(0, s.length() - in.nextInt());
+                st.push(s);
+            } else if (t == 3) {
+                System.out.println(s.charAt(in.nextInt() - 1));
+            } else {
+                st.pop();
+                s = st.peek();
             }
         }
     }
-
 }
