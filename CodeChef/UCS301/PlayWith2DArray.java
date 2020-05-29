761932135a410a40cd4d5a47bc884ea7547bf575
rachit2501 <rac.sri25@gmail.com>
2020-05-29 08:32:50 +0530

    bug fixed

--- CodeChef/UCS301/PlayWith2DArray.java
+++ CodeChef/UCS301/PlayWith2DArray.java
@@ -15,72 +15,73 @@ public class PlayWith2DArray {
         arr[i][j] = sc.nextInt();
       }
     }
-    for (int i = 0; i < n - 1; i++) {
+    for (int i = 0; i < n ; i++) {
       row(arr, n, i);
       column(arr, n, i);
     }
     System.out.println(count);
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
-        System.out.print(arr[i][j] + " ");
-      }
-    }
+    // for (int i = 0; i < n; i++) {
+    //   for (int j = 0; j < n; j++) {
+    //     System.out.print(arr[i][j] + " ");
+    //   }
+    // }
   }
 
   static void row(int arr[][], int n, int row) {
     int x, y, min, yHold = 0;
-    boolean yes = false;
+    
     for (x = 0; x < n; x++) {
+      boolean yes = false;
       min = arr[row][x];
+      
       for (y = row; y < n; y++) {
         if (arr[y][x] < min) {
           min = arr[y][x];
           yHold = y;
-          yes = !yes;
+          yes = true;
         }
       }
       if (yes) {
         int temp = arr[row][x];
         arr[row][x] = min;
         arr[yHold][x] = temp;
-        yes = !yes;
         count++;
       }
     }
-    // for (int i = 0; i < n; i++) {
-    //     for (int j = 0; j < n; j++) {
-    //      System.out.print(arr[i][j] + " ");
-    //     }
-    //   }
-    //   System.out.println("row done");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+         System.out.print(arr[i][j] + " ");
+        }
+        System.out.println();
+      }
+      System.out.println("row done");
 
   }
 
   static void column(int arr[][], int n, int column) {
     int x, y, min, yHold = 0;
-    boolean yes = false;
     for (x = 0; x < n; x++) {
+      boolean yes = false;
       min = arr[x][column];
       for (y = column; y < n; y++) {
         if (arr[x][y] < min) {
           min = arr[x][y];
           yHold = y;
-          yes = !yes;
+          yes = true;
         }
       }
       if (yes) {
         int temp = arr[x][column];
         arr[x][column] = min;
         arr[x][yHold] = temp;
-        yes = !yes;
         count++;
       }
     }
-    // for (int i = 0; i < n; i++) {
-    //     for (int j = 0; j < n; j++) {
-    //      System.out.print(arr[i][j] + " ");
-    //     }
-    //   }
-    //   System.out.println("column done");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+         System.out.print(arr[i][j] + " ");
+        } System.out.println();
+      }
+      System.out.println("column done");
   }
 }
