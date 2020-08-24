7aaadf818fa8cdb71585598026b4546cc9ad0fe7
rachit2501 <rac.sri25@gmail.com>
2020-08-24 18:01:37 +0530

    binary bucket

---
+++ Important/MatrixMedian.java
@@ -0,0 +1,80 @@
+// Given a matrix of integers A of size N x M in which each row is sorted.
+
+// Find an return the overall median of the matrix A.
+
+// Note: No extra memory is allowed.
+
+// Note: Rows are numbered from top to bottom and columns are numbered from left to right.
+
+
+
+// Input Format
+
+// The first and only argument given is the integer matrix A.
+// Output Format
+
+// Return the overall median of the matrix A.
+// Constraints
+
+// 1 <= N, M <= 10^5
+// 1 <= N*M  <= 10^6
+// 1 <= A[i] <= 10^9
+// N*M is odd
+// For Example
+
+// Input 1:
+//     A = [   [1, 3, 5],
+//             [2, 6, 9],
+//             [3, 6, 9]   ]
+// Output 1:
+//     5
+// Explanation 1:
+//     A = [1, 2, 3, 3, 5, 6, 6, 9, 9]
+//     Median is 5. So, we return 5.
+
+// Input 2:
+//     A = [   [5, 17, 100]    ]
+// Output 2:
+//     17 ``` Matrix=
+
+public class Solution {
+
+  public int findMedian(int[][] A) {
+    int m = A.length, n = A[0].length;
+    int min = 0, max = 0;
+
+    for (int i = 0; i < m; i++) {
+      min = Math.min(min, A[i][0]);
+      max = Math.max(max, A[i][n - 1]);
+    }
+
+    int median = (1 + m * n) / 2;
+
+    while (min < max) {
+      int count = 0
+      int mid = min + (max - min) / 2;
+
+      for (int i = 0; i < m; i++) {
+        int found = Arrays.binarySearch(A[i], mid);
+
+        if (found < 0) {
+          // insertion value returned in this case ( Read the docs)
+
+          found = Math.abs(found) - 1;
+        } else {
+          // there is no gaurantee that its the only recurrence or its the first recurrence
+
+          while (found < n && A[i][found] == mid) {
+            found++;
+          }
+        }
+
+        count += found;
+      }
+
+      if (count < median) min = mid + 1; else max = mid;
+    }
+
+    return min;
+  }
+}
---
+++ InterviewBit/BinarySearch/MedianOfArray.java
@@ -0,0 +1,56 @@
+public class Solution {
+
+  public double findMedianSortedArrays(
+    final List<Integer> a,
+    final List<Integer> b
+  ) {
+    int low = 0;
+    List<Integer> X = a.size() < b.size() ? a : b;
+    List<Integer> Y = a.size() < b.size() ? b : a;
+
+    int countUpToFirstElement = (a.size() + b.size() + 1) / 2;
+
+    int partitionX = (low + X.size() - 1) / 2;
+    while (X.size() > 0 && low <= partitionX) {
+      int partitionY = countUpToFirstElement - partitionX - 2;
+      int rightX = partitionX + 1 < X.size()
+        ? X.get(partitionX + 1)
+        : Integer.MAX_VALUE;
+      int rightY = partitionY + 1 < Y.size()
+        ? Y.get(partitionY + 1)
+        : Integer.MAX_VALUE;
+
+      if (X.get(partitionX) <= rightY && Y.get(partitionY) <= rightX) {
+        if ((a.size() + b.size()) % 2 == 1) {
+          return Math.max(X.get(partitionX), Y.get(partitionY));
+        } else {
+          return (
+            (
+              Math.max(X.get(partitionX), Y.get(partitionY)) +
+              Math.min(rightX, rightY)
+            ) /
+            2.0
+          );
+        }
+      } else if (X.get(partitionX) > rightY) {
+        partitionX = partitionX - 1;
+      } else {
+        partitionX = partitionX + 1;
+      }
+    }
+    if (Y.size() == 0) {
+      return -1;
+    } else if ((a.size() + b.size()) % 2 == 1) {
+      return Y.get(countUpToFirstElement - 1);
+    } else {
+      int rightX = X.size() > 0 ? X.get(0) : Integer.MAX_VALUE;
+      return (
+        (
+          Y.get(countUpToFirstElement - 1) +
+          Math.min(rightX, Y.get(countUpToFirstElement))
+        ) /
+        2.0
+      );
+    }
+  }
+}
