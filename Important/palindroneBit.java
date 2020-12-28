a8163e307a87083be775c21071db75fc2842b4d4
rachit2501 <rac.sri25@gmail.com>
2020-12-29 03:09:13 +0530

    upload

---
+++ Important/palindroneBit.java
@@ -0,0 +1,43 @@
+// 2^((l-1)/2) number of items in a group
+// A << x is equal to multiplication by pow(2, x). Think why. 1 << x is equal to pow(2, x).
+
+public class Solution {
+    public int solve(int A) {
+        int count = 1;
+        int len = 1;
+        
+        while(count<A){
+            len ++;
+            int elementsForThisLen = (1 << (len-1)/2);
+            count += elementsForThisLen;
+        }
+        
+        count -= (1<<(len-1)/2); // removing last entry, now I am at the begining of the group to which the element belongs
+        
+        int offSet =  A- count - 1; //found position offset
+    
+        int ans = (1<<(len-1));
+        ans |= (offSet << (len/2)); // len/2 since if the length is for eg 8, then first 4 are unique ( palindrome)
+        
+        int valForReverse = (ans >> (len/2)); // for getting nor reveresed right side
+        
+        int rev = getRev(valForReverse);
+        
+        ans |= rev;
+        
+        return ans;
+    }
+    
+    public static int getRev(int valForReverse){
+        int rev = 0;
+        
+          while (valForReverse != 0){
+            int lowestBit = (valForReverse&1);
+            rev |= lowestBit;
+            rev <<= 1;
+            valForReverse>>=1;
+        }
+        rev >>=1;
+        return rev;
+    }
+}
---
+++ InterviewBit/BitManupulation/numberOf1bits.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int numSetBits(long a) {
+        StringBuilder s = new StringBuilder();
+        
+        while(a > 0){
+            long d = a%2;
+            a = a/2;
+            s.insert(0,d);
+        }
+        
+        int count = 0;
+        for(int x =0; x<s.length();x++){
+            if(s.charAt(x) == '1'){
+                count++;
+            }
+        }
+        return count;
+    }
+}
