53d09d46d0ef5f5072c3aea4839b7b6cbb7f83d8
rachit2501 <rac.sri25@gmail.com>
2020-12-30 01:50:42 +0530

    twopointer

---
+++ InterviewBit/TwoPointer/containerWithMostWater.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int maxArea(ArrayList<Integer> A) {
+        int low = 0;
+        int high = A.size() -1;
+        int maxArea=0;
+        
+        while(low < high){
+            if(A.get(low) < A.get(high)){
+                  maxArea = Math.max( maxArea , A.get(low) * ( high - low ) ) ;
+                low++;
+            }
+            else {
+                  maxArea = Math.max( maxArea , A.get(high) * ( high - low ) ) ;
+                  high--;
+            }
+        }
+        
+        return maxArea;
+    }
+}
---
+++ InterviewBit/TwoPointer/removeDuplicates.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int removeDuplicates(ArrayList a) {
+    int size = a.size();
+    int curIndex=0;
+    
+        for(int i=0; i<size; i++){
+            if(!(a.get(i).equals(a.get(curIndex)))){
+                a.set(++curIndex, a.get(i));
+        }
+       
+    }
+    return curIndex+1;
+    }
+    }
+    
