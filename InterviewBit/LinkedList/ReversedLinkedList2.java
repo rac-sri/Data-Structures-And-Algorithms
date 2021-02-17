344a065392668bc788dfc4f23b1820350c6a1ee0
rachit2501 <rac.sri25@gmail.com>
2021-02-17 06:48:55 +0530

    reveredlinkedlist2

---
+++ InterviewBit/LinkedList/ReversedLinkedList2.java
@@ -0,0 +1,87 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     ListNode(int x) { val = x; next = null; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseBetween(ListNode A, int B, int C) {
+        ListNode head = A; // 1 2 3 4 5
+        ListNode reverse = A;
+        
+        int nodeNumber = C-B;
+        
+        for(int x=1;x<B-1;x++){
+            reverse = reverse.next;
+        }
+        
+        ListNode start = reverse;
+        // start contains the leftside of the list
+        
+        if(nodeNumber == 0){
+            return head;
+        }
+        
+        if(nodeNumber!=0){
+            
+        reverse = reverse.next;
+    
+        // reverse is at the start of the reverse array
+        
+        for(int x = 1; x<= nodeNumber;x++){
+            reverse = reverse.next;
+        }
+       
+        
+
+        ListNode prev;
+        
+        if(B==1)
+         prev = reverse(start, nodeNumber);
+        else {
+            prev = reverse(start.next,nodeNumber);
+        }
+          
+        if(B == 1){
+            start = prev;
+            head = start;
+        }
+        else {
+      
+        start.next = prev;
+        }
+        
+        
+        }
+        
+        
+        return head;
+    }
+    
+    
+    ListNode reverse(ListNode start , int nodeNumber){
+        ListNode prev = null;
+        ListNode current = start;
+        ListNode temp = start;
+        ListNode next = null;
+        
+              
+        while(nodeNumber-->=0){
+            //System.out.println(current.next.val);
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+            if(nodeNumber == 0){
+                // ListNode(int x) { val = x; next = null; }
+                // ListNode right = new ListNode
+                // System.out.println(current.next.val + " " +prev.val + " " + temp.val);
+                temp.next = current.next;
+            }
+        }
+        
+        return prev;
+    }
+}
---
+++ daa/0-1knapsach.java
@@ -0,0 +1,25 @@
+class Knapsach {
+    static int max(int ax, int b){
+        return (a>b)?a:b;
+    }
+
+    static int knapSach(int W , int wt[], int val[], int n){
+        if(n==0 || w==0)
+            return 0;
+
+        if(wt[n-1] > W)
+            return knapSack(W,wt,val,n-1);
+
+        else 
+            return max(val[n-1]+knapSach(W-wt[n-1],wt,val,n-1),knapSack(W,wt,val,n-1));
+    }
+
+    public static void main(String args[]) 
+    { 
+        int val[] = new int[] { 60, 100, 120 }; 
+        int wt[] = new int[] { 10, 20, 30 }; 
+        int W = 50; 
+        int n = val.length; 
+        System.out.println(knapSack(W, wt, val, n)); 
+    } 
+}
